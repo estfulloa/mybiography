d96041af994077bf299f48a1b3ded70cf1dfef3e
Estefania Ulloa <ulloa.aurora.estefania@gmail.com>
2020-11-07 13:47:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mybiography/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mybiography
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_bio_layout)
+    }
+}
